5d51845ffa5ea6ad350d15643eb60249600da191
valercheck <mypowerismind@gmail.com>
2016-06-08 23:50:20 +0300

    Add Notes Repository to Domain

---
+++ SharedNotes.Domain/INoteRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedNotes.Domain
+{
+    public interface INoteRepository
+    {
+        void AddNote(Note note);
+        void DeleteNote(Note note);
+        Note GetNoteById(Guid id);
+        List<Note> GetAllNotes();
+    }
+}
---
+++ SharedNotes.Domain/NotesRepository.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedNotes.Domain
+{
+    public class NotesRepository : INoteRepository
+    {
+        private readonly WorkSpaceContext _dbContext = new WorkSpaceContext();
+
+        public void AddNote(Note note)
+        {
+            _dbContext.Notes.Add(note);
+        }
+
+        public Note GetNoteById(Guid id)
+        {
+            return _dbContext.Notes.FirstOrDefault(note => note.Id.Equals(id));
+        }
+
+        public void DeleteNote(Note note)
+        {
+            _dbContext.Notes.Remove(note);
+        }
+
+        public List<Note> GetAllNotes()
+        {
+            return _dbContext.Notes.ToList();
+        }
+    }
+}
