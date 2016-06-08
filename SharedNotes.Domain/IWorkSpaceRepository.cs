185e474b1ed61585f2c447b44bc480edb526b13b
valercheck <mypowerismind@gmail.com>
2016-06-08 23:59:53 +0300

    Add WorkSpace repository and it's interface

---
+++ SharedNotes.Domain/IWorkSpaceRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SharedNotes.Domain
+{
+    public interface IWorkSpaceRepository
+    {
+        void AddWorkSpace(WorkSpace workspace);
+        void DeleteWorkSpace(WorkSpace workspace);
+        WorkSpace GetWorkSpaceById(Guid id);
+        List<WorkSpace> GetAllWorkSpaces();
+    }
+}
---
+++ SharedNotes.Domain/WorkSpaceRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SharedNotes.Domain
+{
+    public class WorkSpaceRepository : IWorkSpaceRepository
+    {
+        private readonly WorkSpaceContext _dbContext = new WorkSpaceContext();
+
+        public void AddWorkSpace(WorkSpace workspace)
+        {
+            _dbContext.WorkSpaces.Add(workspace);
+        }
+
+        public WorkSpace GetWorkSpaceById(Guid id)
+        {
+            return _dbContext.WorkSpaces.FirstOrDefault(ws => ws.Id.Equals(id));
+        }
+
+        public void DeleteWorkSpace(WorkSpace workspace)
+        {
+            _dbContext.WorkSpaces.Remove(workspace);
+        }
+
+        public List<WorkSpace> GetAllWorkSpaces()
+        {
+            return _dbContext.WorkSpaces.ToList();
+        }
+    }
+}
